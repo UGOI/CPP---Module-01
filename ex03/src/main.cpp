f3ff39bddcd4d3060d1c0cc04907176b9cc8e0e2
Stefan Dukic <stefan@Stefans-MacBook-Pro.local>
2023-01-25 12:58:09 +0100

    update tests in main

--- ex03/src/main.cpp
+++ ex03/src/main.cpp
@@ -6,17 +6,17 @@
 
 int main()
 {
-	// Weapon club = Weapon("crude spiked club");
-	// HumanA bob("Bob", club);
-	// bob.attack();
-	// club.setType("some other type of club");
-	// bob.attack();
-
 	Weapon club = Weapon("crude spiked club");
-	HumanB jim("Jim");
-	jim.setWeapon(club);
-	jim.attack();
+	HumanA bob("Bob", club);
+	bob.attack();
 	club.setType("some other type of club");
-	jim.attack();
+	bob.attack();
+
+	Weapon club2 = Weapon("crude spiked club");
+	HumanB jim2("Jim");
+	jim2.setWeapon(club2);
+	jim2.attack();
+	club2.setType("some other type of club");
+	jim2.attack();
 	return 0;
 }
